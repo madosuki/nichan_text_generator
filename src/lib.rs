24f2e45f9152586f6ea27bdc86a285f0ddf77981
madosuki <yurikasamaaikatsu@gmail.com>
2021-02-16 23:16:28 +0900

    fix now

--- src/lib.rs
+++ src/lib.rs
@@ -2,121 +2,110 @@ use std::collections::HashMap;
 use pwhash::unix::crypt;
 use regex::Regex;
 
-use encoding::{Encoding, EncoderTrap, DecoderTrap};
+use encoding::{Encoding, EncoderTrap};
 use encoding::all::WINDOWS_31J;
 
-pub fn create_trip(key: &str) -> String {
+pub enum OldTripDigit {
+    Ten,
+    Eight,
+    None
+}
+
+pub fn create_trip(key: &str, digit: OldTripDigit) -> Option<String> {
+    let old_trip = |bytes: Vec<u8>| -> Option<String> {
+
+        let mut salt_base: Vec<char> = vec!();
+        let mut key_base: Vec<u8> = vec!();
+    
+        let mut count = 0;
+        for i in &bytes {
+
+            key_base.push(i.to_owned());
+
+            if count == 1 || count == 2 {
+                salt_base.push(i.to_owned() as char);
+            }
+
+            if count == 8 {
+                break;
+            }
+
+            count = count + 1;
+        }
+
+
+        let salt_base_string: String = salt_base.iter().collect();
+
+        let prepare1 = 
+            if salt_base_string.len() == 2 {
+                salt_base_string
+            } else { format!("{}H.", salt_base_string) };
+
+        let re = Regex::new(r"[^.-z]").unwrap();
+
+        let my_tr = |a: &str| -> String {
+            let convert_table_dictionary: HashMap<char, char> =
+                [(':', 'A'),
+                 (';', 'B'),
+                 ('<', 'C'),
+                 ('=', 'D'),
+                 ('>', 'E'),
+                 ('?', 'F'),
+                 ('@', 'G'),
+                 ('[', 'a'),
+                 ('\\', 'b'),
+                 (']', 'c'),
+                 ('^', 'd'),
+                 ('_', 'e'),
+                 ('`', 'f')].iter().cloned().collect();
+        
+            let find = |x: char| -> char {
+
+                match convert_table_dictionary.get_key_value(&x) {
+                    Some((_, v)) => { v.to_owned() }
+                    None => x
+                }
+            };
+        
+            a.to_owned().chars().map(|x| find(x)).collect::<String>()
+        };
+    
+        let prepare2 = re.replace_all(&prepare1, ".");
+        let salt = my_tr(&prepare2);
+
+        let _crypted = crypt(&key_base, &salt);
+    
+        let get_result = |a: &str| -> Option<String> {
+            let start = a.len() - 
+                match digit {
+                    OldTripDigit::Ten => 10,
+                    OldTripDigit::Eight => 8,
+                    OldTripDigit::None => 10,
+                };
+            let end = a.len();
+
+            let data = a[start..end].to_owned();
+
+            let result = format!("◆{}", data);
+            Some(result)
+        };
+
+        match _crypted {
+            Ok(v) => { get_result(&v) }
+            Err(_e) => {
+                println!("crypt error: {}", _e);
+                None
+            }
+        }
+    };
+
     let base = key[1..].to_owned();
     let bytes = WINDOWS_31J.encode(&base, EncoderTrap::Strict).unwrap();
 
-    let mut salt_base: Vec<char> = vec!();
-    let mut key_base: Vec<char> = vec!();
-    
-    let mut count = 0;
-    for i in &bytes {
-
-        key_base.push(i.to_owned() as char);
-
-        if count == 1 || count == 2 {
-            salt_base.push(i.to_owned() as char);
-        }
-
-        if count == 8 {
-            break;
-        }
-
-        count = count + 1;
-    }
-
-    // let mut print_count = 0;
-    // for i in &salt_base {
-    //     println!("count: {}, salt char: {}", print_count, i);
-    //     print_count = print_count + 1;
-    // }
-    
-    // let decoded_salt =
-    //     match WINDOWS_31J.decode(&salt_base, DecoderTrap::Strict) {
-    //         Ok(v) => v,
-    //         Err(_e) => {
-    //             println!("decode error: {}", _e);
-    //             "".to_owned()
-    //         }
-    //     };
-
-    // if decoded == "" {
-    //     return "".to_owned()
-    // }
-
-    // let trip_key = decoded;
-    let trip_key: String = key_base.iter().collect();
-
-    // let salt_base_test_decode = String::from_utf8_lossy(&salt_base);
-    // let salt_base_string = salt_base_test_decode.as_ref().to_owned();
-
-    let salt_base_string: String = salt_base.iter().collect();
-
-    println!("salt_base size: {}", salt_base.len());
-    println!("salt: {}, size: {}", salt_base_string, salt_base_string.len());
-    println!("tripe key: {}", trip_key);
-    
-    let prepare1 =
-        if salt_base_string.len() == 2 {
-            salt_base_string
-        } else { format!("{}H.", salt_base_string) };
-
-    let re = Regex::new(r"[^.-z]").unwrap();
-
-    let my_tr = |a: &str| -> String {
-        let convert_table_dictionary: HashMap<char, char> =
-            [(':', 'A'),
-             (';', 'B'),
-             ('<', 'C'),
-             ('=', 'D'),
-             ('>', 'E'),
-             ('?', 'F'),
-             ('@', 'G'),
-             ('[', 'a'),
-             ('\\', 'b'),
-             (']', 'c'),
-             ('^', 'd'),
-             ('_', 'e'),
-             ('`', 'f')].iter().cloned().collect();
-        
-        let find = |x: char| -> char {
-
-            match convert_table_dictionary.get_key_value(&x) {
-                Some((_, v)) => { v.to_owned() }
-                None => x
-            }
-        };
-        
-        a.to_owned().chars().map(|x| find(x)).collect::<String>()
-    };
-    
-    let prepare2 = re.replace_all(&prepare1, ".");
-    let salt = my_tr(&prepare2);
-    // let salt = salt_base_string;
-
-    println!("final salt: {}", salt);
-    println!("final salt size: {}", salt.len());
-
-    let _crypted = crypt(&trip_key, &salt);
-    
-    let get_result = |a: &str| -> String {
-        let start = a.len() - 10;
-        let end = a.len();
-
-        let data = a[start..end].to_owned();
-
-        format!("◆{}", data)
-    };
-
-    match _crypted {
-        Ok(v) => { get_result(&v) }
-        Err(_e) => {
-            println!("crypt error: {}", _e);
-            "".to_owned()
-        }
+    if bytes.len() < 12  {
+        old_trip(bytes)
+    } else {
+        None
     }
 }
 
@@ -129,7 +118,10 @@ mod tests {
     
     #[test]
     fn test_create_trip() {
-        assert_eq!(create_trip("#istrip"), "◆/WG5qp963c");
-        assert_eq!(create_trip("#ニコニコ"), "◆pA8Bpf.Qvk");
+        let first_result = "◆/WG5qp963c".to_owned();
+        assert_eq!(create_trip("#istrip", OldTripDigit::Ten), Some(first_result));
+
+        let second_result = "◆pA8Bpf.Qvk".to_owned();
+        assert_eq!(create_trip("#ニコニコ", OldTripDigit::Ten), Some(second_result));
     }
 }
