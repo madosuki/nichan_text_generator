62c7b96fc2b4aa0ff0aa91cca98e761d1213b321
madosuki <yurikasamaaikatsu@gmail.com>
2021-02-18 12:52:53 +0900

    implement apply_dice function

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ use pwhash::unix::crypt;
 use base64;
 use regex::Regex;
 
+use rand::Rng;
+
 use encoding::{Encoding, EncoderTrap};
 use encoding::all::WINDOWS_31J;
 
@@ -140,7 +142,28 @@ pub fn create_id(date_time: NaiveDateTime, bbs_key: &str, ip_addr: &str, secret_
 }
 
 pub fn apply_dice(text: &str) -> String {
-    "".to_string()
+    let re = Regex::new(r"!([0-9]{1,3})[dD]([0-9]{1,4})").unwrap();
+
+    let tmp = text.to_owned();
+
+    let replace_result = re.replace_all(&tmp, |cap: &regex::Captures| {
+        let left_str = cap[1].to_owned();
+        let right_str = cap[2].to_owned();
+
+        let target: i32 = cap[2].parse().unwrap();
+        let mut roll_max: i32 = cap[1].parse().unwrap();
+        let mut result_number: i32 = 0;
+        while roll_max != 0 {
+            result_number += rand::thread_rng().gen_range(0..(target + 1));
+            roll_max -= 1;
+        }
+
+        let final_replace_string = format!("【{}D{}: {}】", left_str, right_str, result_number).to_owned();
+
+        final_replace_string
+    });
+
+    replace_result.to_owned().to_string()
 }
 
 #[cfg(test)]
@@ -163,5 +186,11 @@ mod tests {
     fn test_create_id() {
         let date = NaiveDateTime::parse_from_str("2021/02/17 18:01:23", "%Y/%m/%d %H:%M:%S").unwrap();
         assert_eq!(create_id(date, "key", "127.0.0.1", "test_secret_key"), "JBs13t0r");
+    }
+
+    #[test]
+    fn test_apply_dice() {
+        let re = Regex::new(r"!([0-9]{1,3})[dD]([0-9]{1,4})").unwrap();
+        assert_eq!(re.is_match(&apply_dice("!1d100")), false);
     }
 }
